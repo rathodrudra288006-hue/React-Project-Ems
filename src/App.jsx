8d067d0777842f44e19eb99c959ff472603dab28
rathodrudra288006-hue <rathodrudra288006@gamil.com>
2026-09-19 17:10:32 +0530

    today second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Login from "./components/Auth/Login";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Login />
+    </>
+  );
 };
 export default App;
---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("E-mail is ", email);
+    console.log("Password is ", password);
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border-2 rounded-xl border-emerald-600 p-20">
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex flex-col items-center justify-center"
+        >
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+            className="  outline-none bg-transparent border-2 border-emerald-600 py-3 text-xl px-5 rounded-full placeholder:text-gray-400"
+            type="email"
+            placeholder="Enter Your Email"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            className=" outline-none bg-transparent border-2 border-emerald-600 py-3 text-xl px-5 rounded-full mt-3 placeholder:text-gray-400"
+            type="password"
+            placeholder="Enter Password"
+          />
+          <button className=" text-white outline-none border-none bg-emerald-600 py-3 text-xl px-5 rounded-full mt-5 placeholder:text-white">
+            {" "}
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
