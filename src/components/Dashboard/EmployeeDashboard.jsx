f7834cb12de81dea3bf28a4a0f76d36ab30bce02
Rudra Rathod <rathodrudra288006@gmail.com>
2026-09-20 14:41:13 +0530

    Update EmployeeDashboard to add an empty div
    Refactor EmployeeDashboard component to include an empty div.

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 const EmployeeDashboard = () =>{
   return (
-    <div>EmployeeDashboard</div>
+    <div>
+      <div></div>
+    </div>
   )
 }
 export default EmployeeDashboard
