0fa9708875b83879f2e03894e5b15be821f9684d
Rudra Rathod <rathodrudra288006@gmail.com>
2026-09-26 21:10:25 +0530

    Implement submitHandler in CreateTask component
    Added a submitHandler function for form submission.

--- src/components/other/CreateTask.jsx
+++ src/components/other/CreateTask.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 const CreateTask = () => {
+
+  const submitHandler = () => {
+    
+  }
+  
   return (
     <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
       <form className="flex flex-wrap w-full items-start justify-between">
